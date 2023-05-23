72ff567fd688c10a9ccb5aca84b42c08221c2219
patrykmisiewicz <patrykmisiewicz1@gmail.com>
2023-05-23 21:19:45 +0200

    Sum two numbers function

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+let firstNumber = null;
+
 document.addEventListener('DOMContentLoaded', () => {
     const outcomeElement = document.querySelector('.outcome');
   
     const buttons = document.querySelectorAll('.buttons button:not(.operator, .tool)');
     const commaButton = document.querySelector('.comma');
-    const clearButton = document.querySelector('.tool');
+    const clearButton = document.querySelector('.clear');
   
     buttons.forEach(button => {
       button.addEventListener('click', () => {
@@ -18,18 +20,34 @@ document.addEventListener('DOMContentLoaded', () => {
         updateOutcome(buttonText);
       }
     });
-  
+
+    const addButton = document.querySelector('.operator.add');
+
+    addButton.addEventListener('click', () => {
+      firstNumber = parseFloat(outcomeElement.textContent);
+      outcomeElement.textContent = '0';
+    });
+      
     clearButton.addEventListener('click', () => {
       outcomeElement.textContent = '0';
     });
   
     function updateOutcome(text) {
-      if (outcomeElement.textContent === '0' && text !== ',') {
-        outcomeElement.textContent = text;
-      } else if (text === ',' && outcomeElement.textContent.includes(',')) {
-        return;
-      } else {
-        outcomeElement.textContent += text;
+        if (outcomeElement.textContent === '0' && text !== ',') {
+          outcomeElement.textContent = text;
+        } else if (text === ',' && outcomeElement.textContent.includes(',')) {
+          return;
+        } else if (outcomeElement.textContent.length < 12) {
+          outcomeElement.textContent += text;
+        }
       }
-    }
+
+    const equalButton = document.querySelector('.operator.equal');
+    
+    equalButton.addEventListener('click', () => {
+      const secondNumber = parseFloat(outcomeElement.textContent);
+      const result = firstNumber + secondNumber;
+      outcomeElement.textContent = result.toString();
+    }); 
   });
+ 
